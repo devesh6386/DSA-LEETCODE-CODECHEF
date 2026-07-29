6dfd01ad1df94a1a5ac8d3a9cd975829181f6f61
devesh6386 <deveshg6386@gmail.com>
2026-07-29 11:03:53 +0530

    Time: 20 ms (6.30%) | Memory: 12.2 MB (88.83%) - LeetSync

---
+++ 214-shortest-palindrome/shortest-palindrome.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        int n=s.length();
+        string rev=s;
+        reverse(rev.begin(),rev.end());
+        for(int i=0;i<n;i++){
+            //if(s[i]==rev[i])return s;
+            if((memcmp(s.c_str()+0,rev.c_str()+i,n-i)==0)){
+                return rev.substr(0,i)+s;
+            }
+        }
+        return rev+s;
+    }
+};
