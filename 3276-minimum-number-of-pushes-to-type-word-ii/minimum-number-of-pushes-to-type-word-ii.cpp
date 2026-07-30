4c238c34be8900edcd4379d4d41616f3836d0e9c
devesh6386 <deveshg6386@gmail.com>
2026-07-30 11:02:34 +0530

    Time: 4 ms (86.51%) | Memory: 25.4 MB (72.45%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int>freq(26,0);
+        int ans=0;
+        for(char c:word){
+            freq[c-'a']++;
+        }
+        sort(freq.begin(),freq.end(),greater<int>());
+        for(int i=0;i<26;i++){
+            ans+=(freq[i]*((i/8)+1));
+        }
+        return ans;
+    }
+};
+    
