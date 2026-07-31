a9269000cba8add0ab22d9ed5b63e068d9b22f27
devesh6386 <deveshg6386@gmail.com>
2026-07-31 10:41:40 +0530

    Time: 8 ms (61.48%) | Memory: 25.4 MB (84.45%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int minimumPushes(string word) {
         vector<int>freq(26,0);
-        int ans=0;
         for(char c:word){
             freq[c-'a']++;
         }
+        int ans=0;
         sort(freq.begin(),freq.end(),greater<int>());
         for(int i=0;i<26;i++){
             ans+=(freq[i]*((i/8)+1));
