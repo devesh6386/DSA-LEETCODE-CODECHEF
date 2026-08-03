cb5703a3fdca8c6470d2fda59e86176f2e2a9c81
devesh6386 <deveshg6386@gmail.com>
2026-08-04 00:39:39 +0530

    Time: 24 ms (67.80%) | Memory: 140.1 MB (57.49%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.cpp
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -3,18 +3,17 @@ public:
     int solve(int i,int n,vector<int>&stoneValue,vector<int>&dp){
         if(i>=n)return 0;
         if(dp[i]!=-1)return dp[i];
-        int ans=INT_MIN;
         int curr=0;
-        for(int k=0;k<3;k++){//1st,2nd,3rd
-            if(i+k<n){//limit
-                curr+=stoneValue[i+k];//sum of 
-                int margin=curr-solve(i+k+1,n,stoneValue,dp);//difference 
-                ans=max(margin,ans);
+        int ans=INT_MIN;
+        for(int k=0;k<3;k++){
+            if(i+k<n){
+                curr+=stoneValue[i+k];
+                int margin=curr-solve(i+k+1,n,stoneValue,dp);
+                ans=max(ans,margin);
             }
         }
         return dp[i]=ans;
-    };
-    
+    }
     string stoneGameIII(vector<int>& stoneValue) {
         int n=stoneValue.size();
         vector<int>dp(n+1,-1);
@@ -23,4 +22,6 @@ public:
         else if(alic==0)return "Tie";
         return "Bob";
     }
-};
+
+};    
+        
