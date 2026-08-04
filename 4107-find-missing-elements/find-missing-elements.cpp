692e34be473509d44f591129118b9ebbab4fc5ed
devesh6386 <deveshg6386@gmail.com>
2026-08-04 10:54:19 +0530

    Time: 7 ms (18.86%) | Memory: 33.8 MB (17.56%) - LeetSync

---
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ans;
+        int mx=*max_element(nums.begin(),nums.end());
+        int mn=*min_element(nums.begin(),nums.end());
+        unordered_set<int>st(nums.begin(),nums.end());
+        for(int i=mn;i<=mx;i++){
+            if(st.find(i)==st.end()){
+                ans.push_back(i);
+            }
+        }
+        
+        return ans;
+    }
+};
+            
