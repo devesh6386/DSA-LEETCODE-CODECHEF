434ab7478405b2028492f2228c5b7ac82e5cc0cc
devesh6386 <deveshg6386@gmail.com>
2026-08-15 10:42:58 +0530

    Time: 0 ms (100.00%) | Memory: 171.2 MB (56.25%) - LeetSync

---
+++ 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int longestSubsequence(vector<int>& nums) {
+        int n=nums.size();
+        int val=0;
+        for(int num:nums){
+            val=val^num;
+        }
+        if(val!=0)return n;
+        for(int num:nums){
+            if(num!=0){
+                return n-1;
+            }
+        }
+        return 0;
+    }
+};
+           
