c1b175435e976b952bc104f20f91d726de76b17f
devesh6386 <deveshg6386@gmail.com>
2026-08-19 10:46:41 +0530

    Time: 12 ms (93.41%) | Memory: 60.2 MB (68.07%) - LeetSync

--- 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
@@ -2,35 +2,30 @@ class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
         int m=reservedSeats.size();
-        int ans=INT_MIN;
-        unordered_map<int,vector<bool>>isres;
-        for(auto &seat:reservedSeats){
-            int rowno=seat[0];
-            int colno=seat[1];
-            if (isres.find(rowno)==isres.end()){
-                isres[rowno]=vector<bool>(11,false);
-            }
-            isres[rowno][colno]=true;
-            
+        unordered_map<int,int>mpp;
+        //store bitmask
+
+        for(auto &seats:reservedSeats){
+            int rowno=seats[0];
+            int colno=seats[1];
+            mpp[rowno]|=(1<<colno);
         }
-        int total=2*n;
-        for(auto &pair:isres){
-            auto &row=pair.second;
-            //checl for free
-            bool isleftfree=!row[2] && !row[3] && !row[4] && !row[5];
-            bool ismidfree=!row[4] && !row[5] && !row[6] && !row[7];
-            bool isrigfree=!row[6] && !row[7] && !row[8] && !row[9];
-            if(isleftfree && isrigfree){
-                total;
+        int res=2*(n-mpp.size());
+        int maskA=(1<<2)|(1<<3)|(1<<4)|(1<<5);
+        int maskb=(1<<4)|(1<<5)|(1<<6)|(1<<7);
+        int maskc=(1<<6)|(1<<7)|(1<<8)|(1<<9);
+        for(auto &[row,ismask]:mpp){
+            bool grpa=(maskA & ismask)==0;
+            bool grpb=(maskb & ismask)==0;
+            bool grpc=(maskc & ismask)==0;
+            if(grpa && grpc){
+                res+=2;
             }
-            else if(isleftfree || isrigfree || ismidfree){
-                total-=1;
-            }
-            else{
-                total-=2;
+            else if(grpa || grpb || grpc){
+                res+=1;
             }
         }
-        return total;
+        return res;
     }
 };
-            
+           
