f62b3998fadff03918fa3620a4df4d13ce23ae37
devesh6386 <deveshg6386@gmail.com>
2026-08-21 14:57:53 +0530

    Time: 163 ms (51.16%) | Memory: 20.6 MB (46.51%) - LeetSync

--- 3375-kth-smallest-amount-with-single-denomination-combination/kth-smallest-amount-with-single-denomination-combination.cpp
+++ 3375-kth-smallest-amount-with-single-denomination-combination/kth-smallest-amount-with-single-denomination-combination.cpp
@@ -1,48 +1,44 @@
 class Solution {
 public:
 typedef long long ll;
-long long count(ll x,vector<int>&coins){
-    int n=coins.size();
-    ll ans=0;
-    for(int mask=1;mask<(1<<n);mask++){
-        ll l=1;
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            if(mask & (1<<i)){
-                cnt++;
-                l=std::lcm(l,coins[i]);
+    long long count(ll x,vector<int>&coins){
+        ll ans=0;
+        int n=coins.size();
+        for(int mask=1;mask<(1<<n);mask++){
+            ll l=1;
+            int cnt=0;
+            for(int i=0;i<n;i++){
+                if(mask &(1<<i)){
+                    cnt++;
+                    l=std::lcm(l,(ll)coins[i]);
+                }
+                if(l>x)break;
             }
-            
-            if(l>x)break;
+            if(l>x)continue;
+            if(cnt % 2==1){
+                ans+=floor(x/l);
+            }
+            else{
+                ans-=floor(x/l);
+            }
         }
-        if(l>x)continue;
-        if(cnt%2==1){//odd
-            ans+=floor(x/l);
-        }
-        else{
-            ans=ans-floor(x/l);
-        }
+        return ans;
+
     }
-    return ans;
-}
     long long findKthSmallest(vector<int>& coins, int k) {
         int n=coins.size();
-        //lcm
-        //binary seach on ans
-        ll low=0;
-        ll res=0;
+        ll l=0;
         ll r=1LL*(*min_element(coins.begin(),coins.end()))*k;
-        while(low<r){
-            ll mid=low+(r-low)/2;
+        while(l<r){
+            ll mid=l+(r-l)/2;
             if(count(mid,coins)>=k){
-                
                 r=mid;
             }
             else{
-                low=mid+1;
+                l=mid+1;
             }
         }
-        return low;
+        return l;
     }
 };
            
